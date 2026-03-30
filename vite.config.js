6f1f9ff63680b535248455f2413e2fee7c539962
mobartgrafik <mobart.grafik1@gmail.com>
2026-03-30 15:20:52 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,22 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  logLevel: 'error', // Suppress warnings, only show errors
+  logLevel: 'error',
 
   preview: {
-    host: true,
-    allowedHosts: "all"
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: [
+      'mobart.onrender.com'
+    ]
   },
 
   server: {
-    host: true
+    host: '0.0.0.0'
   },
 
   plugins: [
     base44({
-      // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
-      // can be removed if the code has been updated to use the new SDK imports from @base44/sdk
       legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
       hmrNotifier: true,
       navigationNotifier: true,
