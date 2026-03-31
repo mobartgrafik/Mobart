7282dc5ec1b1f52909eb38ba66f95035591ef8b8
mobartgrafik <mobart.grafik1@gmail.com>
2026-03-31 09:05:18 +0200

    Update ClientDetail.jsx

--- src/pages/ClientDetail.jsx
+++ src/pages/ClientDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { base44 } from "@/api/base44Client";
+import { supabase } from "@/supabase";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft, Phone, Mail, Pencil, Plus, FileText } from "lucide-react";
