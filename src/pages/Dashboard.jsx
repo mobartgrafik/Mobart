a735d427650093e1a120a1829967790dba8bec7c
mobartgrafik <mobart.grafik1@gmail.com>
2026-03-31 09:19:14 +0200

    Refactor Dashboard component to use Supabase

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { base44 } from "@/api/base44Client";
+import { supabase } from "@/supabase";
 import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import { createPageUrl } from "@/utils";
